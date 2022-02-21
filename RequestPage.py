893b2524b961f97e979a2bfc226ec708114569ba
urvashi2004 <99309831+urvashi2004@users.noreply.github.com>
2022-02-21 20:35:11 +0530

    Update RequestPage.py

--- RequestPage.py
+++ RequestPage.py
@@ -5,8 +5,7 @@ root = Tk()
 root.geometry("1280x720")
 root.title("Request To Add Page")
 root.configure(bg = "#5E17EB")
-
-type_list = ["Movie","Web Series"]
+root.resizable(False, False)
 
 title_var = StringVar()
 year_var = StringVar()
@@ -24,13 +23,15 @@ def submit():
     year_var.set("")
     type1_var.set("Select An Option")
 
+head_label = Label(root, text = "Request To Add", justify = "center", font = ("Arial", 25))
+
 title_label = Label(root, text = "Title : ", font = ("CinzelDecorative-Bold", 25))
 title_entry = Entry(root, textvariable = title_var, font = ("Arial", 25))
 
 year_label = Label(root, text = "Year : ", font = ("Arial", 25))
 year_entry = Entry(root, textvariable = year_var, font = ("Arial", 25))
 
-type_menu = OptionMenu(root, type1_var, *type_list)
+type_menu = OptionMenu(root, type1_var, "Movie", "Web Series")
 type_menu.pack()
 
 type_label = Label(root, text = "Type : ", font = ("Arial", 25))
@@ -41,16 +42,18 @@ type_entry = Entry(root, textvariable = type_var, font = ("Arial", 25))
 sub_button = Button(root, text = "Submit Entry", font = ("Arial", 25), command = submit)
 sub_button.pack()
 
-title_label.place(x=125, y=125, width=200, height=75)
-title_entry.place(x=400, y=125, width=750, height=75)
+head_label.place(x=125, y=40, width=1025, height=75)
 
-year_label.place(x=125, y=285, width=200, height=75)
-year_entry.place(x=400, y=285, width=750, height=75)
+title_label.place(x=125, y=175, width=200, height=75)
+title_entry.place(x=400, y=175, width=750, height=75)
 
-type_label.place(x=125, y=445, width=200, height=75)
-type_menu.place(x=400, y=445, width=750, height=75)
+year_label.place(x=125, y=335, width=200, height=75)
+year_entry.place(x=400, y=335, width=750, height=75)
 
-sub_button.place(x=460, y=560, width=457, height=75)
+type_label.place(x=125, y=475, width=200, height=75)
+type_menu.place(x=400, y=475, width=750, height=75)
+
+sub_button.place(x=460, y=590, width=457, height=75)
 
 root.mainloop()
 
