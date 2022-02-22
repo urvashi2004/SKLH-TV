22e668c27a81a7397a40b3b4a356e6f792dc0bf7
urvashi2004 <99309831+urvashi2004@users.noreply.github.com>
2022-02-22 21:44:12 +0530

    Update YourContentOutputPage.py

--- YourContentOutputPage.py
+++ YourContentOutputPage.py
@@ -1,5 +1,20 @@
 from tkinter import *
 from  tkinter import ttk
+import mysql.connector as ms
+
+db = ms.connect(host="localhost", user="root", passwd="urvashi22")
+
+if db.is_connected():
+    print("Connection Established")
+else:
+    print("Connection Not Connected")
+
+cs = db.cursor()
+
+cs.execute("use sklh_tv;")
+cs.execute("select * from content;")
+
+data = cs.fetchall()
 
 root  = Tk()
 
@@ -19,34 +34,41 @@ game_frame.place(x=125, y=150, width=1025, height=500)
 game_scroll = Scrollbar(game_frame)
 game_scroll.pack(side=RIGHT, fill=Y)
 
-my_game = ttk.Treeview(game_frame,yscrollcommand=game_scroll.set)
+my_game = ttk.Treeview(game_frame, yscrollcommand=game_scroll.set)
 my_game.pack()
+my_game.place(x=125, y=150, width=1060, height=500)
 
 game_scroll.config(command=my_game.yview)
 
 #define our column
- 
-my_game['columns'] = ('player_id', 'player_name', 'player_Rank', 'player_states', 'player_city')
+my_game['columns'] = ('col_id', 'col_name', 'col_type', 'col_year', 'col_region','col_genre','col_summary')
 
 # format our column
 my_game.column("#0", width=0,  stretch=NO)
-my_game.column("player_id",anchor=CENTER, width=80)
-my_game.column("player_name",anchor=CENTER,width=80)
-my_game.column("player_Rank",anchor=CENTER,width=80)
-my_game.column("player_states",anchor=CENTER,width=80)
-my_game.column("player_city",anchor=CENTER,width=80)
+my_game.column("col_id",anchor=CENTER, width=50)
+my_game.column("col_name",anchor=CENTER,width=200)
+my_game.column("col_type",anchor=CENTER,width=60)
+my_game.column("col_year",anchor=CENTER,width=50)
+my_game.column("col_region",anchor=CENTER,width=100)
+my_game.column("col_genre",anchor=CENTER,width=100)
+my_game.column("col_summary",anchor=CENTER,width=445)
 
 #Create Headings 
 my_game.heading("#0",text="",anchor=CENTER)
-my_game.heading("player_id",text="ID",anchor=CENTER)
-my_game.heading("player_name",text="NAME",anchor=CENTER)
-my_game.heading("player_Rank",text="YEAR",anchor=CENTER)
-my_game.heading("player_states",text="TYPE",anchor=CENTER)
-my_game.heading("player_city",text="SUMMARY",anchor=CENTER)
+my_game.heading("col_id",text="ID",anchor=CENTER)
+my_game.heading("col_name",text="NAME",anchor=CENTER)
+my_game.heading("col_type",text="TYPE",anchor=CENTER)
+my_game.heading("col_year",text="YEAR",anchor=CENTER)
+my_game.heading("col_region",text="REGION",anchor=CENTER)
+my_game.heading("col_genre",text="GENRE",anchor=CENTER)
+my_game.heading("col_summary",text="SUMMARY",anchor=CENTER)
 
 #add data 
-my_game.insert(parent='',index='end',iid=0,text='',
-values=('1','Ninja','101','Oklahoma', 'Moore'))
+for i in range(len(data)):
+    for j in data:
+        my_game.insert(parent='', index='end', iid=i, text='', values=j)
+'''my_game.insert(parent='',index='end',iid=0,text='',
+values=('M101','Your Name','Movie',2016,'Japan','Fantasy','Two teenagers share a profound, magical connection upon discovering they are swapping bodies. Things manage to become even more complicated when the boy and girl decide to meet in person.'))
 my_game.insert(parent='',index='end',iid=1,text='',
 values=('2','Ranger','102','Wisconsin', 'Green Bay'))
 my_game.insert(parent='',index='end',iid=2,text='',
@@ -66,7 +88,10 @@ values=('9','Trink','109','Ohio' , 'Cleveland'))
 my_game.insert(parent='',index='end',iid=9,text='',
 values=('10','Twitch','110','Georgia' , 'Duluth'))
 my_game.insert(parent='',index='end',iid=10,text='',
-values=('11','Animus','111', 'Connecticut' , 'Hartford'))
+values=('11','Animus','111', 'Connecticut' , 'Hartford'))'''
 my_game.pack()
+
+button_back = Button(root, text="BACK", font = ("Arial", 20), borderwidth=0)
+button_back.place(x=5 ,y=5, width=100, height=60)
 
 root.mainloop()
