59e2d535da1c75ca07ca6d89c0d00a3a2b8d3162
urvashi2004 <99309831+urvashi2004@users.noreply.github.com>
2022-02-22 21:43:10 +0530

    Update RequestPage.py

--- RequestPage.py
+++ RequestPage.py
@@ -21,39 +21,34 @@ def submit():
 
     title_var.set("")
     year_var.set("")
-    type1_var.set("Select An Option")
+    type1_var.set("")
 
 head_label = Label(root, text = "Request To Add", justify = "center", font = ("Arial", 25))
-
-title_label = Label(root, text = "Title : ", font = ("CinzelDecorative-Bold", 25))
-title_entry = Entry(root, textvariable = title_var, font = ("Arial", 25))
-
-year_label = Label(root, text = "Year : ", font = ("Arial", 25))
-year_entry = Entry(root, textvariable = year_var, font = ("Arial", 25))
-
-type_menu = OptionMenu(root, type1_var, "Movie", "Web Series")
-type_menu.pack()
-
-type_label = Label(root, text = "Type : ", font = ("Arial", 25))
-'''
-type_entry = Entry(root, textvariable = type_var, font = ("Arial", 25))
-'''
-
-sub_button = Button(root, text = "Submit Entry", font = ("Arial", 25), command = submit)
-sub_button.pack()
-
 head_label.place(x=125, y=40, width=1025, height=75)
 
+title_label = Label(root, text = "Title : ", font = ("CinzelDecorative-Bold", 25))
 title_label.place(x=125, y=175, width=200, height=75)
+
+title_entry = Entry(root, textvariable = title_var, font = ("Arial", 25))
 title_entry.place(x=400, y=175, width=750, height=75)
 
+year_label = Label(root, text = "Year : ", font = ("Arial", 25))
 year_label.place(x=125, y=335, width=200, height=75)
+
+year_entry = Entry(root, textvariable = year_var, font = ("Arial", 25))
 year_entry.place(x=400, y=335, width=750, height=75)
 
-type_label.place(x=125, y=475, width=200, height=75)
+type_menu = OptionMenu(root, type1_var, "Movie", "Web Series")
 type_menu.place(x=400, y=475, width=750, height=75)
 
+type_label = Label(root, text = "Type : ", font = ("Arial", 25))
+type_label.place(x=125, y=475, width=200, height=75)
+
+sub_button = Button(root, text = "Submit Entry", font = ("Arial", 25), command = submit)
 sub_button.place(x=460, y=590, width=457, height=75)
+
+button_back = Button(root, text="BACK", font = ("Arial", 20), borderwidth=0)
+button_back.place(x=5 ,y=5, width=100, height=60)
 
 root.mainloop()
 
