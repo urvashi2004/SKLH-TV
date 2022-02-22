3ad482a08bc87d3c55c09e85f56b0da6dea6ccd4
urvashi2004 <99309831+urvashi2004@users.noreply.github.com>
2022-02-22 21:41:41 +0530

    Update Genre Page.py

--- Genre Page.py
+++ Genre Page.py
@@ -10,22 +10,28 @@ root.config(bg = "#5E17EB")
 label_heading = Label(root, text = "SELECT GENRE", justify = "center", font = ("Arial", 25))
 label_heading.place(x=125, y=40, width=1025, height=75)
 
-button_1 = Button(root, text = "Action", font='CinzelDecorative-Bold 40')
-button_1.place(x=100, y=220, width=263, height=200)
+button_1 = Button(root, text = "Action", font = ("Arial", 25))
+button_1.place(x=110, y=200, width=263, height=150)
 
-button_2 = Button(root, text = "Comedy", font='CinzelDecorative-Bold 40')
-button_2.place(x=470, y=220, width=263, height=200)
+button_2 = Button(root, text = "Comedy", font = ("Arial", 25))
+button_2.place(x=480, y=200, width=263, height=150)
 
-button_3 = Button(root, text = "Horror", font='CinzelDecorative-Bold 40')
-button_3.place(x=850, y=220, width=263, height=200)
+button_3 = Button(root, text = "Horror", font = ("Arial", 25))
+button_3.place(x=860, y=200, width=263, height=150)
 
-button_4 = Button(root, text = "Romance", font='CinzelDecorative-Bold 40')
-button_4.place(x=100, y=480, width=263, height=200)
+button_4 = Button(root, text = "Romance", font = ("Arial", 25))
+button_4.place(x=110, y=380, width=263, height=150)
 
-button_5 = Button(root, text = "Fantasy", font='CinzelDecorative-Bold 40')
-button_5.place(x=470, y=480, width=263, height=200)
+button_5 = Button(root, text = "Fantasy", font = ("Arial", 25))
+button_5.place(x=480, y=380, width=263, height=150)
 
-button_6 = Button(root, text = "Romance", font='CinzelDecorative-Bold 40')
-button_6.place(x=850, y=480, width=263, height=200)
+button_6 = Button(root, text = "Romance", font = ("Arial", 25))
+button_6.place(x=860, y=380, width=263, height=150)
+
+button_back = Button(root, text="BACK", font = ("Arial", 20), borderwidth=0)
+button_back.place(x=5 ,y=5, width=100, height=60)
+
+button_ok = Button(root, text="OK", font = ("Arial", 20), borderwidth=0)
+button_ok.place(x=125, y=600, width=1025, height=75)
 
 root.mainloop()
