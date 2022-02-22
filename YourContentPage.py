c1f6fdb22462197f2f3614117750fb532b0c1e8b
urvashi2004 <99309831+urvashi2004@users.noreply.github.com>
2022-02-22 21:45:00 +0530

    Update YourContentPage.py

--- YourContentPage.py
+++ YourContentPage.py
@@ -5,6 +5,7 @@ root = Tk()
 root.geometry("1280x720")
 root.title("Your Profile Page")
 root.configure(bg = "#5E17EB")
+root.resizable(False, False)
 
 type_label = Label(root, text = "Your Profile", font = ("Arial", 25))
 type_label.place(x=125, y=40, width=1025, height=75)
@@ -17,5 +18,8 @@ watch_but.place(x=700, y=220, width=500, height=200)
 
 done_but = Button(root, text = "Already Watched", font = ("Arial", 25))
 done_but.place(x=400, y=480, width=500, height=200)
+
+button_back = Button(root, text="BACK", font = ("Arial", 20), borderwidth=0)
+button_back.place(x=5 ,y=5, width=100, height=60)
 
 root.mainloop()
