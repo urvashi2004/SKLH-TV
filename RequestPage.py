33511481be616479d9576a67e5efb6920c8a00c4
urvashi2004 <99309831+urvashi2004@users.noreply.github.com>
2022-02-26 09:26:37 +0530

    Update RequestPage.py

--- RequestPage.py
+++ RequestPage.py
@@ -1,59 +1,76 @@
 from tkinter import *
+from tkinter import messagebox
+import mysql.connector as ms
 
-root = Tk()
+def RequestPageFunc():
+    db = ms.connect(host="localhost",user="root",passwd="urvashi22")
 
-root.geometry("1280x720")
-root.title("Request To Add Page")
-root.configure(bg = "#5E17EB")
-root.resizable(False, False)
+    if db.is_connected():
+        print("Connection Established")
+    else:
+        print("Connection Not Connected")
 
-title_var = StringVar()
-year_var = StringVar()
-type1_var = StringVar()
-option_var = IntVar()
+    cs = db.cursor()
+    cs.execute("use sklh_tv;")
 
-def submit():
-    title = title_var.get()
-    year = year_var.get()
-    type1 = type1_var.get()
+    root = Tk()
 
-    print("The title of the {} is '" + title + "' made in " + year.format(type1))
+    root.geometry("1280x720")
+    root.title("Request To Add Page")
+    root.configure(bg = "#5E17EB")
+    root.resizable(False, False)
 
-    title_var.set("")
-    year_var.set("")
-    type1_var.set("")
+    title_var = StringVar()
+    year_var = StringVar()
+    type1_var = StringVar()
 
-head_label = Label(root, text = "Request To Add", justify = "center", font = ("Arial", 25))
-head_label.place(x=125, y=40, width=1025, height=75)
+    def submit():
+        title = title_var.get()
+        year = year_var.get()
+        type1 = type1_var.get()
 
-title_label = Label(root, text = "Title : ", font = ("CinzelDecorative-Bold", 25))
-title_label.place(x=125, y=175, width=200, height=75)
+        messagebox.showinfo("Request Sent", title.title() + " made in " + year + " is requested successfully.")
 
-title_entry = Entry(root, textvariable = title_var, font = ("Arial", 25))
-title_entry.place(x=400, y=175, width=750, height=75)
+        cs.execute("insert into request values ('" + title.title() + "','" + year + "','" + type1 + "');")
 
-year_label = Label(root, text = "Year : ", font = ("Arial", 25))
-year_label.place(x=125, y=335, width=200, height=75)
+        title_var.set("")
+        year_var.set("")
+        type1_var.set("")
 
-year_entry = Entry(root, textvariable = year_var, font = ("Arial", 25))
-year_entry.place(x=400, y=335, width=750, height=75)
+        root.destroy()
+        from HomePageChoices import HomePageChoicesFunc
+        HomePageChoicesFunc()
 
-type_menu = OptionMenu(root, type1_var, "Movie", "Web Series")
-type_menu.place(x=400, y=475, width=750, height=75)
+    head_label = Label(root, text = "Request To Add", justify = "center", font = ("Arial", 25))
+    head_label.place(x=125, y=40, width=1025, height=75)
 
-type_label = Label(root, text = "Type : ", font = ("Arial", 25))
-type_label.place(x=125, y=475, width=200, height=75)
+    title_label = Label(root, text = "Title : ", font = ("CinzelDecorative-Bold", 25))
+    title_label.place(x=125, y=175, width=200, height=75)
 
-sub_button = Button(root, text = "Submit Entry", font = ("Arial", 25), command = submit)
-sub_button.place(x=460, y=590, width=457, height=75)
+    title_entry = Entry(root, textvariable = title_var, font = ("Arial", 25))
+    title_entry.place(x=400, y=175, width=750, height=75)
 
-button_back = Button(root, text="BACK", font = ("Arial", 20), borderwidth=0)
-button_back.place(x=5 ,y=5, width=100, height=60)
+    year_label = Label(root, text = "Year : ", font = ("Arial", 25))
+    year_label.place(x=125, y=335, width=200, height=75)
 
-root.mainloop()
+    year_entry = Entry(root, textvariable = year_var, font = ("Arial", 25))
+    year_entry.place(x=400, y=335, width=750, height=75)
 
-'''
-https://www.geeksforgeeks.org/tkinter-optionmenu-widget/
+    type_menu = OptionMenu(root, type1_var, "Movie", "Web Series")
+    type_menu.place(x=400, y=475, width=750, height=75)
 
-root.bind('<Return>',selecttest1)
-'''
+    type_label = Label(root, text = "Type : ", font = ("Arial", 25))
+    type_label.place(x=125, y=475, width=200, height=75)
+
+    sub_button = Button(root, text = "Submit Entry", font = ("Arial", 25), command = lambda:submit())
+    sub_button.place(x=460, y=590, width=457, height=75)
+
+    def Click():
+        root.destroy()
+        from HomePageChoices import HomePageChoicesFunc
+        HomePageChoicesFunc()
+
+    button_back = Button(root, text="BACK", font = ("Arial", 20), borderwidth=0, command = lambda:Click())
+    button_back.place(x=5 ,y=5, width=100, height=60)
+
+    root.mainloop()
